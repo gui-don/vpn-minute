e60c3c2bc3265d08038140bd2dee6adf737867e1
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-07-12 23:54:17 -0400

    feat: adds a ContainerLoader class to fetch config, compile and return container

---
+++ src/Core/DI/ContainerLoader.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VPNMinute\Core\DI;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ContainerLoader
+{
+    const SERVICE_CONFIGURATION_FILE = __DIR__.'/../../../config/service/services.yml';
+
+    public function load(): ContainerBuilder
+    {
+        $containerBuilder = new ContainerBuilder();
+
+        $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__));
+        $loader->load(self::SERVICE_CONFIGURATION_FILE);
+
+        $containerBuilder->compile();
+
+        return $containerBuilder;
+    }
+}
